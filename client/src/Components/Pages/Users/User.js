7a61ab89145b07a4ded5cf4c8142db22ef535063
Ivan <tanyakupach@gmail.com>
2021-12-29 08:54:19 +0200

    Add profile, message, img

--- client/src/Components/Pages/Users/User.js
+++ client/src/Components/Pages/Users/User.js
@@ -4,10 +4,18 @@ import { NavLink } from "react-router-dom";
 import UserTags from "./UserTags";
 
 const User = ({ user }) => {
+  console.log(user.picture);
   return (
     <div key={user._id} className={styles.user}>
       <NavLink to={`user/${user.name.split(" ").join("-")}/${user.sub}`}>
-        <img src={user.picture} alt="" />
+        <img
+          src={
+            user.picture
+              ? user.picture
+              : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPyGNr2qL63Sfugk2Z1-KBEwMGOfycBribew&usqp=CAU"
+          }
+          alt=""
+        />
       </NavLink>{" "}
       <div>
         <NavLink to={`user/${user.name.split(" ").join("-")}/${user.sub}`}>
--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,7 @@
 import * as axios from "axios";
 const instance = axios.create({
-  baseURL: process.env.BASEURL || "https://forumintelligent.herokuapp.com/api",
+  // baseURL: process.env.BASEURL || "https://forumintelligent.herokuapp.com/api",
+  baseURL: process.env.BASEURL || "http://localhost:5000/api",
   // baseURL: `https://forumintelligent.herokuapp.com/api`,
 });
 export const usersAPI = {
